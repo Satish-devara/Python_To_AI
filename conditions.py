0d49593845dcd446cb6f62b4f9977c195dc6befd
Satish <sd1761256@gmail.com>
2026-09-15 20:15:23 +0530

    learning list

---
+++ conditions.py
@@ -0,0 +1,8 @@
+marks = 70
+
+if marks >= 90:
+    print("Excellent")
+elif marks >= 80:
+    print("Good")
+else:
+    print("Fail")
---
+++ list.py
@@ -0,0 +1,11 @@
+number = [10, 20, 30 , 50]
+
+number.append(40)
+print(number)
+number.remove(40)
+print(number)
+
+print(len(number))
+
+if 20 in number:
+    print("found")
